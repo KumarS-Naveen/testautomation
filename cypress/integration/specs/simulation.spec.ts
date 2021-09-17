e813d867a2e21d366f4e163969069af689f179bf
NaveenKumarS <naveenkumar.s@siemens.com>
2021-09-17 11:59:37 +0530

    init

--- cypress/integration/specs/simulation.spec.ts
+++ cypress/integration/specs/simulation.spec.ts
@@ -6,7 +6,7 @@ import { startTransaction } from '../../page-objects/start-transaction';
 import { stopTransaction } from '../../page-objects/stop-transaction';
 import {ChargerNotification } from '../../page-objects/charger-notification';
 import { Utils } from '../../page-objects/Utils'
-
+import { meterValuepagess } from '../../page-objects/metervalue-page'
 
 const simulate = new SimulatorPage();
 const statusNotification = new StausNotificationPage();
@@ -16,7 +16,7 @@ const startTransactionPage = new startTransaction();
 const stopTransactionPage = new stopTransaction();
 const errorNotification = new ChargerNotification();
 const utils = new Utils();
-
+const meterValuepage = new meterValuepagess();
 describe('Simulator Navigation', () => {
     it('Simulator',() => {
         const loop = 100;
@@ -50,7 +50,7 @@ describe('Simulator Navigation', () => {
             startTransactionPage.start("1");
             meterValue.sendmetervalue("1","SoC",String(initialSOC));
             cy.wait(90000);
-            meterValue.sendmetervalue("1","SoC",String(initialSOC+ utils.getRandomInt(25,35)));
+            meterValuepage.sendMeterValue();
             cy.wait(70000);
             meterValue.sendmetervalue("1","SoC",String(initialSOC+ utils.getRandomInt(60,90)));
             cy.wait(2000); 
@@ -79,4 +79,192 @@ describe('Simulator Navigation', () => {
         }
 
     });
+
+    it('Simulator1',() => {
+        const loop = 100;
+        const initialSOC = 5;
+        //connect
+        cy.visit('https://ebusdev-simulator-ebusdev.eu1.mindsphere.io/');
+        simulate.chargerurl().clear().type('wss://ebuspt-chargerapp-ebusprod.eu1.mindsphere.io/ChargingStations/ebuspt-66940013ad2ac1c103fa80e2c23f6bbf/da4a475');
+        simulate.changeIDtag().clear().type('Automation');
+        simulate.chargerType("Sicharge_UC_Charger").click();
+        simulate.connect().click();
+        
+        for(let i = 0; i < loop; i++){
+        
+        //status change
+            statusNotification.setChargerAsAvailable();
+            statusNotification.setConnectorAsAvailable("1"); 
+            statusNotification.setConnectorAsPreparing("1");
+            
+            
+            statusNotification.setConnectorAsAvailable("2");
+            statusNotification.setConnectorAsPreparing("2");
+            
+
+            vehicle.sendvehicleID("1","BUSf46ae1d2");
+            vehicle.sendvehicleID("2","BUS996a5232");
+
+            // connector id 1 
+            meterValue.sendmetervalue("1","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("1");
+            startTransactionPage.start("1");
+            meterValue.sendmetervalue("1","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValuepage.sendMeterValue();
+            cy.wait(70000);
+            meterValue.sendmetervalue("1","SoC",String(initialSOC+ utils.getRandomInt(60,90)));
+            cy.wait(2000); 
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("1");
+            //errorNotification.trigger("1");
+
+
+            // connector id 2
+            meterValue.sendmetervalue("2","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("2");
+            startTransactionPage.start("2");
+            meterValue.sendmetervalue("2","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC)+ utils.getRandomInt(40,50));
+            cy.wait(70000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC+ utils.getRandomInt(70,90)));
+            cy.wait(2000);
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("2");
+            //errorNotification.trigger("2");
+            statusNotification.setConnectorAsAvailable("2");
+        }
+
+    });
+
+    it('Simulator2',() => {
+        const loop = 100;
+        const initialSOC = 5;
+        //connect
+        cy.visit('https://ebusdev-simulator-ebusdev.eu1.mindsphere.io/');
+        simulate.chargerurl().clear().type('wss://ebuspt-chargerapp-ebusprod.eu1.mindsphere.io/ChargingStations/ebuspt-66940013ad2ac1c103fa80e2c23f6bbf/da4a475');
+        simulate.changeIDtag().clear().type('Automation');
+        simulate.chargerType("Sicharge_UC_Charger").click();
+        simulate.connect().click();
+        
+        for(let i = 0; i < loop; i++){
+        
+        //status change
+            statusNotification.setChargerAsAvailable();
+            statusNotification.setConnectorAsAvailable("1"); 
+            statusNotification.setConnectorAsPreparing("1");
+            
+            
+            statusNotification.setConnectorAsAvailable("2");
+            statusNotification.setConnectorAsPreparing("2");
+            
+
+            vehicle.sendvehicleID("1","BUSf46ae1d2");
+            vehicle.sendvehicleID("2","BUS996a5232");
+
+            // connector id 1 
+            meterValue.sendmetervalue("1","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("1");
+            startTransactionPage.start("1");
+            meterValue.sendmetervalue("1","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValuepage.sendMeterValue();
+            cy.wait(70000);
+            meterValue.sendmetervalue("1","SoC",String(initialSOC+ utils.getRandomInt(60,90)));
+            cy.wait(2000); 
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("1");
+            //errorNotification.trigger("1");
+
+
+            // connector id 2
+            meterValue.sendmetervalue("2","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("2");
+            startTransactionPage.start("2");
+            meterValue.sendmetervalue("2","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC)+ utils.getRandomInt(40,50));
+            cy.wait(70000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC+ utils.getRandomInt(70,90)));
+            cy.wait(2000);
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("2");
+            //errorNotification.trigger("2");
+            statusNotification.setConnectorAsAvailable("2");
+        }
+
+    });
+
+    it('Simulator3',() => {
+        const loop = 100;
+        const initialSOC = 5;
+        //connect
+        cy.visit('https://ebusdev-simulator-ebusdev.eu1.mindsphere.io/');
+        simulate.chargerurl().clear().type('wss://ebuspt-chargerapp-ebusprod.eu1.mindsphere.io/ChargingStations/ebuspt-66940013ad2ac1c103fa80e2c23f6bbf/da4a475');
+        simulate.changeIDtag().clear().type('Automation');
+        simulate.chargerType("Sicharge_UC_Charger").click();
+        simulate.connect().click();
+        
+        for(let i = 0; i < loop; i++){
+        
+        //status change
+            statusNotification.setChargerAsAvailable();
+            statusNotification.setConnectorAsAvailable("1"); 
+            statusNotification.setConnectorAsPreparing("1");
+            
+            
+            statusNotification.setConnectorAsAvailable("2");
+            statusNotification.setConnectorAsPreparing("2");
+            
+
+            vehicle.sendvehicleID("1","BUSf46ae1d2");
+            vehicle.sendvehicleID("2","BUS996a5232");
+
+            // connector id 1 
+            meterValue.sendmetervalue("1","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("1");
+            startTransactionPage.start("1");
+            meterValue.sendmetervalue("1","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValuepage.sendMeterValue();
+            cy.wait(70000);
+            meterValue.sendmetervalue("1","SoC",String(initialSOC+ utils.getRandomInt(60,90)));
+            cy.wait(2000); 
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("1");
+            //errorNotification.trigger("1");
+
+
+            // connector id 2
+            meterValue.sendmetervalue("2","SoC",String(0));
+            cy.wait(90000);
+            statusNotification.setConnectorAsCharging("2");
+            startTransactionPage.start("2");
+            meterValue.sendmetervalue("2","SoC",String(initialSOC));
+            cy.wait(90000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC)+ utils.getRandomInt(40,50));
+            cy.wait(70000);
+            meterValue.sendmetervalue("2","SoC",String(initialSOC+ utils.getRandomInt(70,90)));
+            cy.wait(2000);
+            stopTransactionPage.stop();
+            cy.wait(2000);
+            statusNotification.setConnectorAsAvailable("2");
+            //errorNotification.trigger("2");
+            statusNotification.setConnectorAsAvailable("2");
+        }
+
+    });
+
+
 }); 
